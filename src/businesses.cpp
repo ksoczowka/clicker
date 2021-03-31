e314cb08136c733327cb31a2994d836b6790a82c
DarkAriesX <skrzys2103@gmail.com>
2021-03-31 02:28:44 +0200

    making Business class and inheritance

--- src/businesses.cpp
+++ src/businesses.cpp
@@ -1,8 +1,9 @@
 #include "businesses.hpp"
 
 Student::Student() {
+    upgradable = false;
     quantity = Quantity();
-    upgrade = Quantity(100, 0, 0, 0, 0, false);
+    upgradeCost = Quantity(100, 0, 0, 0, 0, false);
     border = sf::RectangleShape(sf::Vector2f(350, 100));
     border.setOutlineColor(sf::Color::Black);
     border.setOutlineThickness(5);
@@ -14,11 +15,11 @@ Student::Student() {
 
     status = clickStatus::IDLE;
 }
-void Student::render(sf::RenderTarget* target) {
+void Business::render(sf::RenderTarget* target) {
     target->draw(border);
     target->draw(clickArea);
 }
-void Student::update(sf::Mouse cursor, sf::Window* window) {
+void Business::update(sf::Mouse cursor, sf::Window* window) {
     status = clickStatus::IDLE;
     if(clickArea.getGlobalBounds().contains(sf::Vector2f(cursor.getPosition(*window))) && cursor.isButtonPressed(sf::Mouse::Button::Left)) {
         status = clickStatus::ACTIVE;
--- src/businesses.hpp
+++ src/businesses.hpp
@@ -8,16 +8,49 @@ enum class clickStatus {
     AUTO,
     ACTIVE
 };
-
-class Student {
- public:
-    Student();
+class Business {
+public:
     void render(sf::RenderTarget* target);
     void update(sf::Mouse cursor, sf::Window* window);
- private:
+protected:
+    bool upgradable;
     sf::RectangleShape border;
     sf::CircleShape clickArea;
+    sf::RectangleShape upgradeButton;
     clickStatus status;
     Quantity quantity;
-    Quantity upgrade;
+    Quantity upgradeCost;
+    Quantity upgradeCostAdd;
+    Quantity income;
+    sf::Text quantityText;
+    sf::Text upgradeCostText;
+    sf::Text incomeText;
+};
+class Student : public Business {
+public:
+    Student();    
+};
+class Section : public Business {
+public:
+    Section();    
+};
+class Dojo : public Business {
+public:
+    Dojo();    
+};
+class Club : public Business {
+public:
+    Club();    
+};
+class Branch : public Business {
+public:
+    Branch();    
+};
+class ContOrg : public Business {
+public:
+    ContOrg();    
+};
+class WorldOrg : public Business {
+public:
+    WorldOrg();    
 };
